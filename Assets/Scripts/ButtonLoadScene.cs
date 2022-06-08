b4ff594d51d911f02071a65188643bc91cf2ecf3
Jojo Home PC <robert.gream@yahoo.com.au>
2022-06-09 01:50:50 +0930

    Developed the tutorial world more and added the main menu w/ functional buttons for starting and quitting

---
+++ Assets/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+    public string _sceneName = string.Empty;
+
+    // Start is called before the first frame update
+    public void OnButtonPressed()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
